889e0f72bf463a4666e7cc77d7391769cd88060b
unknown <huline@qq.com>
2018-10-18 20:41:21 +0800

    sdf

--- web.go
+++ web.go
@@ -1,7 +1,33 @@
 package main
 
-import "github.com/astaxie/beego"
+import (
+    "strings"
+    "fmt"
+    "net/http"
+    "log"
+)
+
+func sayHelloName(w http.ResponseWriter, r *http.Request){
+    r.ParseForm()
+    fmt.Println(r.Form)
+    fmt.Println("path: ", r.URL.Path)
+    fmt.Println("scheme: ", r.URL.Scheme)
+    fmt.Println(r.Form["url_long"])
+    for k, v := range r.Form{
+        fmt.Println("key: ", k)
+        fmt.Println("val: ", strings.Join(v, " "))
+    }
+    fmt.Fprintf(w, "hello chain!")
+}
+
+func Start(){
+    http.HandleFunc("/", sayHelloName)
+    err := http.ListenAndServe(":9090", nil)
+    if err != nil{
+        log.Fatal("ListenAndServe: ", err)
+    }
+}
 
 func main(){
-    beego.Run()
+    Start()
 }
